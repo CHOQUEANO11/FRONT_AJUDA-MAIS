03d731fa989d90ccc96fc3853629943dbaa4c204
Nilson Silva <nilsonchoque11@gmail.com>
2025-02-08 17:34:54 -0300

    Connection API RENDER

--- src/app/dashboard/schedule/page.tsx
+++ src/app/dashboard/schedule/page.tsx
@@ -5,7 +5,7 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 
 import { config } from "@/config";
-import Schedule from "@/components/dashboard/Schedule/create-schedule";
+import Schedule from "@/components/dashboard/schedule/create-schedule";
 
 export const metadata = { title: `Agenda | Dashboard | ${config.site.name}` } satisfies Metadata;
 
